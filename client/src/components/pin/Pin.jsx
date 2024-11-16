6e6798e3b647f624640f12d1d04804ac996deaea
Rachit Bhat <bhatrachit29@gmail.com>
2024-11-16 11:49:41 +0530

    w

--- client/src/components/pin/Pin.jsx
+++ client/src/components/pin/Pin.jsx
@@ -3,6 +3,7 @@ import "./pin.scss";
 import { Link } from "react-router-dom";
 
 function Pin({ item }) {
+  console.log(item.images[0]);
   return (
     <Marker position={[item.latitude, item.longitude]}>
       <Popup>
